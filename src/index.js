4c9fe75b29125183513b4ada7b271134fdc3874a
matthewvarghese1 <mtv5115@psu.edu>
2024-10-08 21:52:51 -0400

    l

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://teal-fenglisu-217183.netlify.app', 
+    origin: 'https://335b-206-81-12-171.ngrok-free.app', 
     credentials: true 
 }));
 
